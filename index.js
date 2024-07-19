f02666e7df4103ba5aabeb9410aa0708c81a86c1
sheela mishra <sheelamishra1999@gmail.com>
2024-07-19 20:42:27 +0530

    changes

---
+++ index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    autoplay:{
+      delay:4000,
+
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
